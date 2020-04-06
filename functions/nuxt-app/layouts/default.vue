95b3fbe0c20778decc884cee17ab4f630162a4dd
Satoshi <aki319809@gmail.com>
2020-04-06 10:06:15 +0900

    delete:サイドバーの不要部分削除

--- functions/nuxt-app/layouts/default.vue
+++ functions/nuxt-app/layouts/default.vue
@@ -1,10 +1,9 @@
 <template>
   <v-app dark>
+    <!-- ナビゲーションバー中身 -->
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
       :clipped="clipped"
-      fixed
       app
     >
       <v-list>
@@ -24,61 +23,26 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
+
+    <!-- ヘッダー -->
     <v-app-bar
       :clipped-left="clipped"
       fixed
       app
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
+
+<!-- メイン -->
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+
+    <!-- footer -->
     <v-footer
       :fixed="fixed"
       app
@@ -92,24 +56,32 @@
 export default {
   data () {
     return {
-      clipped: false,
+      clipped: true,
       drawer: false,
       fixed: false,
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'HOME',
           to: '/'
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          title: 'アクセス',
+          to: '/access'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: '器具の使い方',
+          to: '/howtouse'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'フィットネスへの入り方',
+          to: '/enter'
         }
       ],
-      miniVariant: false,
       right: true,
-      rightDrawer: false,
       title: 'Vuetify.js'
     }
   }
